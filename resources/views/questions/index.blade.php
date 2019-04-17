96c4410dc25608e6b0e024ee3ea7ab951dfb9a61
androidborn <tweaksbuzz@gmail.com>
2019-04-17 23:04:06 +0530

    Authorization with the Gates

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -33,11 +33,15 @@
                             <div class="d-flex align-items-lg-end">
                                 <h3 class="mt-0"><a href=" {{ $question->url }} ">{{ $question->title }}</a></h3>
                                 <div class="mt-0">
+                                   @if(Auth::user()->can('update-question', $question))
                                     <a href=" {{ route('questions.edit', $question->id) }} " class="btn btn-sm btn-outline-info">Edit</a>
+                                    @endif
+                                   @if(Auth::user()->can('delete-question', $question))
                                     <form method="post" action=" {{ route('questions.destroy', $question->id)}}">
                                         {{ method_field('DELETE') }}
                                     @csrf
                                     <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure')">Delete</button>
+                                    @endif
                             </form> 
                                 </div>
                             </div>
